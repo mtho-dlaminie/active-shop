4cd59ff79bf34d2b12a58bf7a0a9f75b9fa2bcc3
Mthobisi <30963112+mtho-dlaminie@users.noreply.github.com>
2021-01-13 18:45:12 +0200

    initial

---
+++ src/main/java/com/momentum/activeshop/ActiveShopApplication.java
@@ -0,0 +1,13 @@
+package com.momentum.activeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveShopApplication.class, args);
+	}
+
+}
